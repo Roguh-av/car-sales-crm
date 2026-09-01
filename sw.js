d86f15b2575456a9edde77fd3e819d5b1f8fc6f1
Roguh-av <ironroguh@gmail.com>
2026-09-01 14:39:22 -0400

    Add dashboard calendar view switcher

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='crm-live-v7';
+const VERSION='crm-live-v8';
 self.addEventListener('install',event=>{self.skipWaiting()});
 self.addEventListener('activate',event=>{
   event.waitUntil((async()=>{
