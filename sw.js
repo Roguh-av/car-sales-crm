124ce3acf7a243ef1a1de00770c28fd93e8661e6
Roguh-av <ironroguh@gmail.com>
2026-09-01 13:30:41 -0400

    Add calendar completion checkboxes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='crm-live-v5';
+const VERSION='crm-live-v6';
 self.addEventListener('install',event=>{self.skipWaiting()});
 self.addEventListener('activate',event=>{
   event.waitUntil((async()=>{
