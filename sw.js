efeed41bfe00cda257477689c867c31bfef44db7
Roguh-av <ironroguh@gmail.com>
2026-09-01 16:13:18 -0400

    Add finance customer management

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='crm-live-v9';
+const VERSION='crm-live-v10';
 self.addEventListener('install',event=>{self.skipWaiting()});
 self.addEventListener('activate',event=>{
   event.waitUntil((async()=>{
